d55779acd322b4fd9ac5e6d18d35aca095a7bba3
meganpaffrath <meganpaffrath@gmail.com>
2021-03-06 23:43:33 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sweetwater Is Awesome</title>
+</head>
+<body>
+  <h1>Comment Report:</h1>
+  <?php include "comment_report.php" ?>
+</body>
+</html>
